1cf0972a125d372fe4dbcede3dd91800116f5ab3
nchandur <chandur.nischal@gmail.com>
2025-10-29 13:08:27 -0400

    play a round'

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+	"unicode/utf8"
+
 	"github.com/nchandur/blackjack/models"
 )
 
 func main() {
+
+	message := "Welcome to Blackjack!!!"
+
+	repeated := strings.Repeat("-", utf8.RuneCountInString(message))
+
+	fmt.Println(message)
+	fmt.Println(repeated)
 
 	game := models.NewGame()
 	game.PlayRound()
