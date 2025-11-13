a96b2abaf7e25c5ffb130b47b4e0bdd63f156234
nchandur <chandur.nischal@gmail.com>
2025-11-12 22:21:38 -0500

    bot creation and strategy

--- entities/bot/bot.go
+++ entities/bot/bot.go
@@ -5,21 +5,9 @@ import (
 	"github.com/nchandur/blackjack/game/structs"
 )
 
-var ACTION_MAP = map[byte]uint16{
-	's': 0,
-	'h': 1,
-	'd': 2,
-}
-
-var REVERSE_ACTION_MAP = map[uint16]byte{
-	0: 's',
-	1: 'h',
-	2: 'd',
-}
-
 type Rule struct {
-	BotHand    uint16
-	DealerCard uint16
+	Hand       uint8
+	DealerCard uint8
 	IsSoft     bool
 	Action     byte
 }
