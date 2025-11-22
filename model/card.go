db6c5fe58c961888943b1b779d1af1f75e153ca8
nchandur <chandur.nischal@gmail.com>
2025-11-21 19:20:52 -0500

    added color to cards

--- model/card.go
+++ model/card.go
@@ -2,6 +2,10 @@ package model
 
 import "fmt"
 
+const RED = "\x1b[31m"
+const BLACK = "\x1b[30m"
+const RESET = "\x1b[0m"
+
 type Card struct {
 	Value uint8
 	rank  string
@@ -9,9 +13,20 @@ type Card struct {
 }
 
 func (c *Card) String() []string {
-	return []string{
-		"-----",
-		fmt.Sprintf("│%-2s%s│", c.rank, c.suit),
-		"-----",
+
+	if c.suit == "H" || c.suit == "D" {
+		return []string{
+			fmt.Sprintf("%s-----%s", RED, RESET),
+			fmt.Sprintf("%s│%-2s%s│%s", RED, c.rank, c.suit, RESET),
+			fmt.Sprintf("%s-----%s", RED, RESET),
+		}
+
 	}
+
+	return []string{
+		fmt.Sprintf("%s-----%s", BLACK, RESET),
+		fmt.Sprintf("%s│%-2s%s│%s", BLACK, c.rank, c.suit, RESET),
+		fmt.Sprintf("%s-----%s", BLACK, RESET),
+	}
+
 }
